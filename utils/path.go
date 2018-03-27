f5acdda13eb2a844f328fb72410f1e5e72f66bc5
zhengxiaochuan-3 <zhengxiaochuan-3@163.com>
2018-03-27 16:36:38 +0800

    remove uncalled code

--- utils/path.go
+++ utils/path.go
@@ -1,58 +1,8 @@
 package utils
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"os"
-	"strings"
 )
-
-func substr(s string, pos, length int) string {
-	runes := []rune(s)
-	l := pos + length
-	if l > len(runes) {
-		l = len(runes)
-	}
-	return string(runes[pos:l])
-}
-
-// GetParentFullPath ...
-func GetParentFullPath(in string) (parentFullPath string) {
-	parentFullPath = substr(in, 0, strings.LastIndex(in, "/"))
-	if parentFullPath == "" {
-		parentFullPath = "/"
-	}
-	return
-}
-
-// GetSelfName ...
-func GetSelfName(in string) (selfName string) {
-	tmp := strings.Split(in, "/")
-	selfName = tmp[len(tmp)-1]
-	if selfName == "" {
-		selfName = "/"
-	}
-	return
-}
-
-// GetParentName ...
-func GetParentName(in string) (parentName string) {
-	tmp := strings.Split(in, "/")
-	parentName = tmp[len(tmp)-2]
-	if parentName == "" {
-		parentName = "/"
-	}
-	return
-}
-
-// MD5 ...
-func MD5(in string) string {
-	h := md5.New()
-	h.Write([]byte(in))
-	cipherStr := h.Sum(nil)
-	hexStr := hex.EncodeToString(cipherStr)
-	return hexStr
-}
 
 // LocalPathExists ...
 func LocalPathExists(path string) (bool, error) {
@@ -64,12 +14,4 @@ func LocalPathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
-}
-
-func ConvertValueToArgs(args ...interface{}) []interface{} {
-	var ret []interface{}
-	for _, v := range args {
-		ret = append(ret, v)
-	}
-	return ret
 }
