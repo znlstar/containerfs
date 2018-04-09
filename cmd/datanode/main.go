11e5bd9e1d7623e39e9d8eb76db1fe200423b36f
zhengxiaochuan-3 <zhengxiaochuan-3@163.com>
2018-04-09 11:22:55 +0800

    datanode package optimization

--- cmd/datanode/main.go
+++ cmd/datanode/main.go
@@ -7,7 +7,7 @@ import (
 	"runtime"
 	"strings"
 
-	dt "github.com/tiglabs/containerfs/datanode"
+	"github.com/tiglabs/containerfs/datanode"
 	"github.com/tiglabs/containerfs/logger"
 	"github.com/tiglabs/containerfs/utils"
 )
@@ -17,10 +17,10 @@ func init() {
 	var loglevel string
 	var volMgrHosts string
 
-	flag.StringVar(&dt.DtAddr.Host, "host", "127.0.0.1:8801", "ContainerFS DataNode Host")
-	flag.StringVar(&dt.DtAddr.Tier, "tier", "sas", "ContainerFS DataNode Storage Medium")
-	flag.StringVar(&dt.DtAddr.Path, "datapath", "", "ContainerFS DataNode Data Path")
-	flag.StringVar(&dt.DtAddr.Log, "logpath", "/export/Logs/containerfs/logs/", "ContainerFS Log Path")
+	flag.StringVar(&datanode.DtAddr.Host, "host", "127.0.0.1:8801", "ContainerFS DataNode Host")
+	flag.StringVar(&datanode.DtAddr.Tier, "tier", "sas", "ContainerFS DataNode Storage Medium")
+	flag.StringVar(&datanode.DtAddr.Path, "datapath", "", "ContainerFS DataNode Data Path")
+	flag.StringVar(&datanode.DtAddr.Log, "logpath", "/export/Logs/containerfs/logs/", "ContainerFS Log Path")
 	flag.StringVar(&loglevel, "loglevel", "error", "ContainerFS Log Level")
 	flag.StringVar(&volMgrHosts, "volmgr", "10.8.64.216,10.8.64.217,10.8.64.218", "ContainerFS VolMgr hosts")
 
@@ -32,15 +32,15 @@ func init() {
 
 	tmp := strings.Split(volMgrHosts, ",")
 
-	dt.VolMgrHosts = make([]string, 3)
-	dt.VolMgrHosts[0] = tmp[0] + ":7703"
-	dt.VolMgrHosts[1] = tmp[1] + ":7713"
-	dt.VolMgrHosts[2] = tmp[2] + ":7723"
+	datanode.VolMgrHosts = make([]string, 3)
+	datanode.VolMgrHosts[0] = tmp[0] + ":7703"
+	datanode.VolMgrHosts[1] = tmp[1] + ":7713"
+	datanode.VolMgrHosts[2] = tmp[2] + ":7723"
 
-	dt.DtAddr.Flag = dt.DtAddr.Path + "/.registryflag"
+	datanode.DtAddr.Flag = datanode.DtAddr.Path + "/.registryflag"
 
 	logger.SetConsole(true)
-	logger.SetRollingFile(dt.DtAddr.Log, "datanode.log", 10, 100, logger.MB) //each 100M rolling
+	logger.SetRollingFile(datanode.DtAddr.Log, "datanode.log", 10, 100, logger.MB) //each 100M rolling
 
 	switch loglevel {
 	case "error":
@@ -53,18 +53,18 @@ func init() {
 		logger.SetLevel(logger.ERROR)
 	}
 
-	_, err := os.Stat(dt.DtAddr.Path)
+	_, err := os.Stat(datanode.DtAddr.Path)
 	if err != nil {
-		logger.Error("data node statup failed : dt.DtAddr.Path not exist !")
+		logger.Error("data node statup failed : datanode.DtAddr.Path not exist !")
 		os.Exit(1)
 	}
 
-	dt.RegistryToVolMgr()
+	datanode.RegistryToVolMgr()
 
 }
 
 func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	dt.StartDataService()
+	datanode.StartDataService()
 }
