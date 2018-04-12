59607d519f340a4adaaee95734b6fbbc1c82d879
zhengxiaochuan-3 <zhengxiaochuan-3@163.com>
2018-04-12 22:18:42 +0800

    add Copyright

--- client/fuse/fs.go
+++ client/fuse/fs.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, tig.jd.com. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0 that can be found in the LICENSE file.
+
 package fuse
 
 import (
