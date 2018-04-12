46e748c818b0ee39c685ebd32040fc89ea945dba
seven7777777 <stevecheey@gmail.com>
2018-04-12 05:36:15 -0400

    1.moidfy: add comments and right docs	1

--- raftopt/btreeinstance/btreeinstance.go
+++ raftopt/btreeinstance/btreeinstance.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, tig.jd.com. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0 that can be found in the LICENSE file.
+
 package btreeinstance
 
 import (
@@ -5,15 +8,15 @@ import (
 	"strconv"
 )
 
-// Uint64 ...
+//Uint64 ...
 type Uint64 uint64
 
-// Less returns true if uint64(a) < uint64(b).
+//Less
 func (a Uint64) Less(b btree.Item) bool {
 	return a < b.(Uint64)
 }
 
-// String ...
+//String ...
 type String string
 
 // Less returns true if int(a) < int(b).
@@ -21,35 +24,35 @@ func (a String) Less(b btree.Item) bool {
 	return a < b.(String)
 }
 
-// DentryKV ...
+//DentryKV ...
 type DentryKV struct {
 	K string
 	V []byte
 }
 
-// Less ...
+//Less
 func (a DentryKV) Less(b btree.Item) bool {
 	return a.K < b.(DentryKV).K
 }
 
-// InodeKV ...
+//InodeKV ...
 type InodeKV struct {
 	K uint64
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a InodeKV) Less(b btree.Item) bool {
 	return a.K < b.(InodeKV).K
 }
 
-// BGKV ...
+//BGKV ...
 type BGKV struct {
 	K uint64
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a BGKV) Less(b btree.Item) bool {
 	return a.K < b.(BGKV).K
 }
@@ -62,122 +65,122 @@ type DataNodeKV struct {
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a DataNodeKV) Less(b btree.Item) bool {
 	return a.K < b.(DataNodeKV).K
 }
 
-// DataNodeBGKV ...
+//DataNodeBGKV ...
 type DataNodeBGKV struct {
 	K string
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a DataNodeBGKV) Less(b btree.Item) bool {
 	return a.K < b.(DataNodeBGKV).K
 }
 
-// MetaNodeKV ...
+//MetaNodeKV ...
 type MetaNodeKV struct {
 	K uint64
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a MetaNodeKV) Less(b btree.Item) bool {
 	return a.K < b.(MetaNodeKV).K
 }
 
-// BlockGroupKV ...
+//BlockGroupKV ...
 type BlockGroupKV struct {
 	K uint64
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a BlockGroupKV) Less(b btree.Item) bool {
 	return a.K < b.(BlockGroupKV).K
 }
 
-// MNRGKV ...
+//MNRGKV ...
 type MNRGKV struct {
 	K uint64
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a MNRGKV) Less(b btree.Item) bool {
 	return a.K < b.(MNRGKV).K
 }
 
-// VOLKV ...
+//VOLKV ...
 type VOLKV struct {
 	K string
 	V []byte
 }
 
-// Less ...
+//Less ...
 func (a VOLKV) Less(b btree.Item) bool {
 	return a.K < b.(VOLKV).K
 }
 
-// Key ...
+//Key ...
 func (a DataNodeKV) Key() string {
 	return a.K
 }
 
-// Value ...
+//Value ...
 func (a DataNodeKV) Value() []byte {
 	return a.V
 }
 
-// Key ...
+//Key ...
 func (a DataNodeBGKV) Key() string {
 	return a.Key()
 }
 
-// Value ...
+//Value ...
 func (a DataNodeBGKV) Value() []byte {
 	return a.V
 }
 
-// Key ...
+//Key ...
 func (a MetaNodeKV) Key() string {
 	return strconv.FormatUint(a.K, 10)
 }
 
-// Value ...
+//Value ...
 func (a MetaNodeKV) Value() []byte {
 	return a.V
 }
 
-// Key ...
+//Key ...
 func (a BlockGroupKV) Key() string {
 	return strconv.FormatUint(a.K, 10)
 }
 
-// Value ...
+//Value ...
 func (a BlockGroupKV) Value() []byte {
 	return a.V
 }
 
-// Key ...
+//Key ...
 func (a MNRGKV) Key() string {
 	return strconv.FormatUint(a.K, 10)
 }
 
-// Value ...
+//Value ...
 func (a MNRGKV) Value() []byte {
 	return a.V
 }
 
-// Key ...
+//Key ...
 func (a VOLKV) Key() string {
 	return a.K
 }
 
-// Value ...
+//Value ...
 func (a VOLKV) Value() []byte {
 	return a.V
 }
