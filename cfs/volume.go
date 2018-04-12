62a1a7486bc54d690448664bfb4dc67f2d276869
jimbo17 <jimbo.xia@foxmail.com>
2018-04-12 15:37:55 +0800

    cfs package optimization phase II

--- cfs/volume.go
+++ cfs/volume.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, tig.jd.com. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0 that can be found in the LICENSE file.
+
 package cfs
 
 import (
@@ -12,6 +15,7 @@ import (
 	"golang.org/x/net/context"
 )
 
+// GetAllDatanode to get infos of all Datanodes
 func GetAllDatanode() (int32, []*vp.DataNode) {
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
 	if err != nil {
@@ -21,7 +25,7 @@ func GetAllDatanode() (int32, []*vp.DataNode) {
 	defer conn.Close()
 	vc := vp.NewVolMgrClient(conn)
 	pGetDataNodeReq := &vp.GetDataNodeReq{}
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	pGetDataNodeAck, err := vc.GetDataNode(ctx, pGetDataNodeReq)
 	if err != nil {
 		logger.Error("GetAllDatanode failed,grpc func err :%v", err)
@@ -34,6 +38,7 @@ func GetAllDatanode() (int32, []*vp.DataNode) {
 	return 0, pGetDataNodeAck.DataNodes
 }
 
+// GetAllMetanode to get infos of all Metanodes
 func GetAllMetanode() (int32, []*vp.MetaNode) {
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
 	if err != nil {
@@ -43,7 +48,7 @@ func GetAllMetanode() (int32, []*vp.MetaNode) {
 	defer conn.Close()
 	vc := vp.NewVolMgrClient(conn)
 	pGetAllMetaNodeReq := &vp.GetAllMetaNodeReq{}
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	pGetAllMetaNodeAck, err := vc.GetMetaNode(ctx, pGetAllMetaNodeReq)
 	if err != nil {
 		logger.Error("GetAllMetanode failed,grpc func err :%v", err)
@@ -55,6 +60,8 @@ func GetAllMetanode() (int32, []*vp.MetaNode) {
 	}
 	return 0, pGetAllMetaNodeAck.MetaNodes
 }
+
+// DelDatanode to delete a Datanode from cluster
 func DelDatanode(host string) int {
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
 	if err != nil {
@@ -67,7 +74,7 @@ func DelDatanode(host string) int {
 	pDelDataNodeReq := &vp.DelDataNodeReq{
 		Host: host,
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	ack, err := vc.DelDataNode(ctx, pDelDataNodeReq)
 	if err != nil {
 		logger.Error("DelDataNode failed,grpc func err :%v", err)
@@ -80,7 +87,7 @@ func DelDatanode(host string) int {
 	return 0
 }
 
-// CreateVol volume
+// CreateVol to create a new volume
 func CreateVol(name string, capacity string, tier string, copies string) int32 {
 
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
@@ -98,7 +105,7 @@ func CreateVol(name string, capacity string, tier string, copies string) int32 {
 		Tier:       tier,
 		Copies:     copies,
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	ack, err := vc.CreateVol(ctx, pCreateVolReq)
 	if err != nil {
 		logger.Error("CreateVol failed, VolMgr Leader return failed,  err:%v", err)
@@ -119,107 +126,7 @@ func CreateVol(name string, capacity string, tier string, copies string) int32 {
 	return 0
 }
 
-/*  TODO:
-// Expand volume once for fuseclient
-func ExpandVolRS(UUID string, MtPath string) int32 {
-	path := MtPath + "/expanding"
-
-	fd, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
-	if err != nil {
-		return -2
-	}
-	defer fd.Close()
-
-	conn, err := DialMeta("Cluster")
-	if err != nil {
-		logger.Error("ExpandVolRS failed,Dial to Cluster leader metanode fail :%v", err)
-		return -1
-	}
-	defer conn.Close()
-	mc := mp.NewMetaNodeClient(conn)
-
-	pExpandVolRSReq := &mp.ExpandVolRSReq{
-		VolID: UUID,
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	pExpandVolRSAck, err := mc.ExpandVolRS(ctx, pExpandVolRSReq)
-	if err != nil {
-		logger.Error("ExpandVol once volume:%v failed, Cluster leader metanode return error:%v", UUID, err)
-		os.Remove(path)
-		return -1
-	}
-	if pExpandVolRSAck.Ret == -1 {
-		logger.Error("ExpandVol once volume:%v failed, Cluster leader metanode return -1:%v", UUID)
-		os.Remove(path)
-		return -1
-	} else if pExpandVolRSAck.Ret == 0 {
-		logger.Error("ExpandVol volume:%v once failed, Cluster leader metanode return 0 because volume totalsize not enough expand", UUID)
-		os.Remove(path)
-		return 0
-	}
-
-	out := UpdateMetaForExpandVol(UUID, pExpandVolRSAck)
-
-	if out != 0 {
-		logger.Error("ExpandVol volume:%v once cluster leader metanode success but update volume leader metanode fail, so rollback cluster leader metanode this expand resource", UUID)
-		pDelReq := &mp.DelVolRSForExpandReq{
-			UUID: UUID,
-			BGPS: pExpandVolRSAck.BGPS,
-		}
-
-		pDelAck, err := mc.DelVolRSForExpand(ctx, pDelReq)
-		if err != nil || pDelAck.Ret != 0 {
-			logger.Error("ExpandVol once volume:%v success but update meta failed, then rollback cluster leader metanode error", UUID)
-		}
-		os.Remove(path)
-		return -1
-	}
-
-	os.Remove(path)
-	return 1
-}
-
-func UpdateMetaForExpandVol(UUID string, ack *mp.ExpandVolRSAck) int {
-	var mpBlockGroups []*mp.BlockGroup
-	for _, v := range ack.BGPS {
-		mpBlockGroup := &mp.BlockGroup{
-			BlockGroupID: v.Blocks[0].BGID,
-			FreeSize:     utils.BlockGroupSize,
-		}
-		mpBlockGroups = append(mpBlockGroups, mpBlockGroup)
-	}
-
-	logger.Debug("ExpandVolRS volume:%v to leader metanode BlockGroups Info:%v", UUID, mpBlockGroups)
-	// Meta handle
-	conn2, err := DialMeta(UUID)
-	if err != nil {
-		logger.Error("ExpandVol volume:%v once volmgr success but Dial to metanode fail :%v", UUID, err)
-		return -1
-	}
-	defer conn2.Close()
-
-	mc := mp.NewMetaNodeClient(conn2)
-	pmExpandNameSpaceReq := &mp.ExpandNameSpaceReq{
-		VolID:       UUID,
-		BlockGroups: mpBlockGroups,
-	}
-	ctx2, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	pmExpandNameSpaceAck, err := mc.ExpandNameSpace(ctx2, pmExpandNameSpaceReq)
-	if err != nil {
-		logger.Error("ExpandVol volume:%v once volmgr success but MetaNode return error:%v", UUID, err)
-		return -1
-	}
-	if pmExpandNameSpaceAck.Ret != 0 {
-		logger.Error("ExpandVol volume:%v once volmgr success but MetaNode return not equal 0:%v", UUID)
-		return -1
-	}
-
-	return 0
-}
-*/
-
-// CreateVol volume
+// ExpandVol to expand voume capacity
 func ExpandVol(uuid string, capacity string) int32 {
 
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
@@ -235,7 +142,7 @@ func ExpandVol(uuid string, capacity string) int32 {
 		UUID:  uuid,
 		Space: int32(spaceQuota),
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	ack, err := vc.ExpandVol(ctx, pExpandVolReq)
 	if err != nil {
 		logger.Error("ExpandVol failed, VolMgr Leader return failed,  err:%v", err)
@@ -248,7 +155,7 @@ func ExpandVol(uuid string, capacity string) int32 {
 	return 0
 }
 
-// Migrate bad DataNode blocks data to some Good DataNodes
+// Migrate to migrate blocks of the bad DataNode to some normal DataNodes
 func Migrate(host string) int32 {
 	pMigrateReq := &vp.MigrateReq{
 		DataNodeHost: host,
@@ -260,7 +167,7 @@ func Migrate(host string) int32 {
 	}
 	defer conn.Close()
 	vc := vp.NewVolMgrClient(conn)
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	pMigrateAck, err := vc.Migrate(ctx, pMigrateReq)
 	if err != nil {
 		logger.Error("Migrate failed: %v", err)
@@ -273,6 +180,7 @@ func Migrate(host string) int32 {
 	return 0
 }
 
+// GetAllVolumeInfos to get infos of all volumes
 func GetAllVolumeInfos() (int32, []*vp.Volume) {
 
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
@@ -284,7 +192,7 @@ func GetAllVolumeInfos() (int32, []*vp.Volume) {
 	vc := vp.NewVolMgrClient(conn)
 
 	pVolumeInfosReq := &vp.VolumeInfosReq{}
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	pVolumeInfosAck, err := vc.VolumeInfos(ctx, pVolumeInfosReq)
 	if err != nil {
 		logger.Error("GetAllVolumeInfos failed,grpc func err :%v", err)
@@ -297,7 +205,7 @@ func GetAllVolumeInfos() (int32, []*vp.Volume) {
 	return 0, pVolumeInfosAck.Volumes
 }
 
-// GetVolInfo volume info
+// GetVolInfo to get detail infos of a volume by UUID
 func GetVolInfo(name string) (int32, *vp.GetVolInfoAck) {
 
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
@@ -311,7 +219,7 @@ func GetVolInfo(name string) (int32, *vp.GetVolInfoAck) {
 	pGetVolInfoReq := &vp.GetVolInfoReq{
 		UUID: name,
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	ack, err := vc.GetVolInfo(ctx, pGetVolInfoReq)
 	if err != nil || ack.Ret != 0 {
 		return -1, &vp.GetVolInfoAck{}
@@ -319,7 +227,7 @@ func GetVolInfo(name string) (int32, *vp.GetVolInfoAck) {
 	return 0, ack
 }
 
-//Get blockgroup info
+//GetBlockGroupInfo to get block group infos by block group ID
 func GetBlockGroupInfo(idStr string) (int32, *vp.GetBlockGroupInfoAck) {
 
 	bgID, err := strconv.ParseUint(idStr, 10, 64)
@@ -338,7 +246,7 @@ func GetBlockGroupInfo(idStr string) (int32, *vp.GetBlockGroupInfoAck) {
 	pGetBlockGroupInfoReq := &vp.GetBlockGroupInfoReq{
 		BGID: bgID,
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	ack, err := vc.GetBlockGroupInfo(ctx, pGetBlockGroupInfoReq)
 	if err != nil {
 		logger.Error("GetBlockGroupInfo failed: %v", err)
@@ -351,7 +259,7 @@ func GetBlockGroupInfo(idStr string) (int32, *vp.GetBlockGroupInfoAck) {
 	return 0, ack
 }
 
-// SnapShootVol ...
+// SnapShotVol to snapshot a volume's metadata
 func SnapShotVol(uuid string) int32 {
 	// send to metadata to delete a  map
 
@@ -369,7 +277,7 @@ func SnapShotVol(uuid string) int32 {
 		pmSnapShotNameSpaceReq := &mp.SnapShotNameSpaceReq{
 			VolID: uuid,
 		}
-		ctx, _ := context.WithTimeout(context.Background(), 100*time.Second)
+		ctx, _ := context.WithTimeout(context.Background(), SNAPSHOT_TIMEOUT_SECONDS*time.Second)
 		pmSnapShotNameSpaceAck, err := mc.SnapShotNameSpace(ctx, pmSnapShotNameSpaceReq)
 		if err != nil {
 			logger.Error("SnapShotVol failed,grpc func err :%v", err)
@@ -385,7 +293,7 @@ func SnapShotVol(uuid string) int32 {
 	return 0
 }
 
-//Snapshot cluster data on volmgrs
+// SnapShotCluster to snapshot cluster data on volmgrs
 func SnapShotCluster() int32 {
 
 	for _, v := range VolMgrHosts {
@@ -400,7 +308,7 @@ func SnapShotCluster() int32 {
 
 		vc := vp.NewVolMgrClient(conn)
 		pSnapShotClusterReq := &vp.SnapShotClusterReq{}
-		ctx, _ := context.WithTimeout(context.Background(), 100*time.Second)
+		ctx, _ := context.WithTimeout(context.Background(), SNAPSHOT_TIMEOUT_SECONDS*time.Second)
 		pSnapShotClusterAck, err := vc.SnapShotCluster(ctx, pSnapShotClusterReq)
 		if err != nil {
 			logger.Error("SnapShotVol failed,grpc func err :%v", err)
@@ -416,7 +324,7 @@ func SnapShotCluster() int32 {
 	return 0
 }
 
-// DeleteVol function
+// DeleteVol to delete a volume by UUID
 func DeleteVol(uuid string) int32 {
 
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
@@ -430,7 +338,7 @@ func DeleteVol(uuid string) int32 {
 	pDeleteVolReq := &vp.DeleteVolReq{
 		UUID: uuid,
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	pDeleteVolAck, err := vc.DeleteVol(ctx, pDeleteVolReq)
 	if err != nil {
 		return -1
@@ -444,6 +352,7 @@ func DeleteVol(uuid string) int32 {
 	return 0
 }
 
+// GetVolMetaLeader to get the leader of volume raft group by UUID
 func GetVolMetaLeader(UUID string) (string, error) {
 	_, conn, err := utils.DialVolMgr(VolMgrHosts)
 	if err != nil {
@@ -454,7 +363,7 @@ func GetVolMetaLeader(UUID string) (string, error) {
 	pGetMetaNodeRGReq := &vp.GetMetaNodeRGReq{
 		UUID: UUID,
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), VOLUME_TIMEOUT_SECONDS*time.Second)
 	pGetMetaNodeRGAck, err := vc.GetMetaNodeRG(ctx, pGetMetaNodeRGReq)
 	if err != nil {
 		return "", err
