8dee2f109a264e1f0afc074b3492d469234e9fd8
bjguoweilong <guowL$0917>
2018-04-17 14:12:47 +0800

    init from action_dev branches

--- sdk/vol.go
+++ sdk/vol.go
@@ -1,1 +1,41 @@
 package sdk
+
+import "time"
+
+type Vol struct {
+	VolId uint64
+	Goal  uint8
+	Hosts []string
+	Status uint8
+}
+
+
+const (
+	VolViewUrl="/client/volview"
+)
+
+type VolWraper struct {
+	MasterAddrs []string
+	Vols []*Vol
+}
+
+
+func (wraper *VolWraper)Init(master []string) (err error){
+	return
+}
+
+
+func (wraper *VolWraper)update(){
+	ticker:=time.NewTicker(time.Second*10)
+	for {
+		select {
+			case <-ticker.C:
+				wraper.getVols()
+		}
+	}
+}
+
+
+func (wraper *VolWraper)getVols()(err error){
+	return
+}
