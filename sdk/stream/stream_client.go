0318d78c68f672b9fd9f1c234941d74808b14358
bjguoweilong <guowL$0917>
2018-04-17 14:34:19 +0800

    init from action_dev branches

--- sdk/stream/stream_client.go
+++ sdk/stream/stream_client.go
@@ -1,6 +1,25 @@
 package stream
 
+import (
+	"github.com/tiglabs/containerfs/sdk"
+	"io"
+	"container/list"
+	"sync"
+)
 
-type StreamWrite struct {
+type StreamClient struct {
+	dataList list.Element
+	sync.Mutex
+	wraper *sdk.VolWraper
+}
 
+
+func (sc *StreamClient)StreamWrite(writer io.ReadWriter)(reader io.Reader,err error){
+
+	return
+}
+
+func (sc *StreamClient)StreamRead(writer io.Writer)(reader io.Reader,err error){
+
+	return
 }
